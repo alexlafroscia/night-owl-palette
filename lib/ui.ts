d46ddd429b8bddbabbb376bc4ccc704da830c1a6
Alex LaFroscia <alex@lafroscia.com>
2024-06-19 15:57:19 -0400

    add tab styles

--- lib/ui.ts
+++ lib/ui.ts
@@ -7,6 +7,18 @@ export const dark = {
     foreground: "#ffffffcc",
     // hoverBackground: "#7e57c2",
   },
+
+  tab: {
+    border: "#272B3B",
+    inactive: {
+      background: "#01111d",
+      foreground: "#5f7e97",
+    },
+    active: {
+      background: "#0b2942",
+      foreground: "#d2dee7",
+    },
+  },
 };
 
 export const light = {
@@ -17,5 +29,17 @@ export const light = {
     background: "#2AA298",
     foreground: "#F0F0F0",
     // hoverBackground: "#7e57c2",
+  },
+
+  tab: {
+    border: "#F0F0F0",
+    inactive: {
+      background: "#F0F0F0",
+      foreground: "#403f53",
+    },
+    active: {
+      background: "#F6F6F6",
+      foreground: "#403f53",
+    },
   },
 };
