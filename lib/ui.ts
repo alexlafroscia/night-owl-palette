26208f861c6405c1258e1a940bd09b19f57b56cf
Alex LaFroscia <alex@lafroscia.com>
2024-06-19 22:32:54 -0400

    define activity bar, sidebar and list colors

--- lib/ui.ts
+++ lib/ui.ts
@@ -2,10 +2,45 @@ export const dark = {
   background: "#011627",
   foreground: "#d6deeb",
 
+  activityBar: {
+    background: "#011627",
+    foreground: "#5f7e97",
+    border: "#011627",
+  },
+  activityBarBadge: {
+    background: "#44596b",
+    foreground: "#ffffff",
+  },
+
   button: {
     background: "#7e57c2cc",
     foreground: "#ffffffcc",
     // hoverBackground: "#7e57c2",
+  },
+
+  list: {
+    activeSelection: {
+      background: "#234d708c",
+      foreground: "#ffffff",
+    },
+    focus: {
+      background: "#010d18",
+      foreground: "#ffffff",
+    },
+    hover: {
+      background: "#011627",
+      foreground: "#ffffff",
+    },
+    inactiveSelection: {
+      background: "#0e293f",
+      foreground: "#5f7e97",
+    },
+  },
+
+  sideBar: {
+    background: "#011627",
+    foreground: "#89a4bb",
+    border: "#011627",
   },
 
   tab: {
@@ -25,10 +60,45 @@ export const light = {
   background: "#FBFBFB",
   foreground: "#403f53",
 
+  activityBar: {
+    background: "#F0F0F0",
+    foreground: "#403f53",
+    border: "#F0F0F0",
+  },
+  activityBarBadge: {
+    background: "#403f53",
+    foreground: "#F0F0F0",
+  },
+
   button: {
     background: "#2AA298",
     foreground: "#F0F0F0",
     // hoverBackground: "#7e57c2",
+  },
+
+  list: {
+    activeSelection: {
+      background: "#d3e8f8",
+      foreground: "#403f53",
+    },
+    focus: {
+      background: "#d3e8f8",
+      foreground: "#403f53",
+    },
+    hover: {
+      background: "#d3e8f8",
+      foreground: "#403f53",
+    },
+    inactiveSelection: {
+      background: "#E0E7EA",
+      foreground: "#403f53",
+    },
+  },
+
+  sideBar: {
+    background: "#F0F0F0",
+    foreground: "#403f53",
+    border: "#F0F0F0",
   },
 
   tab: {
